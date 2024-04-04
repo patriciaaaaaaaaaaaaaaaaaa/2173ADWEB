46904070d0feb4bc31e36784f63b55de97470b65
patriciaaaaaaaaaaaaaaaaaa <123538754+patriciaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2024-04-04 23:55:34 +0800

    Add files via upload

--- app.module.ts
+++ app.module.ts
@@ -1,31 +1,55 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { PartnersComponent } from './partners/partners.component';
-import { JoinUsComponent } from './join-us/join-us.component';
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { BookDetailComponent } from './components/book-detail/book-detail.component';
+import { BooksListComponent } from './components/books-list/books-list.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatChipsModule} from '@angular/material/chips';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent,
-    PartnersComponent,
-    JoinUsComponent
+    AddBookComponent,
+    BookDetailComponent,
+    BooksListComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatIconModule,
+    MatChipsModule,
+    MatTabsModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatDatepickerModule,
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
